753bceaa56a3c264b59d5e3734767b005ed99586
Mohamed Khalfella <khalfella@gmail.com>
2020-01-29 22:43:44 +0000

    Framework Ready

--- libdtrace.cpp
+++ libdtrace.cpp
@@ -5,58 +5,65 @@ using namespace v8;
 
 class DTraceConsumer : public Nan::ObjectWrap {
  public:
-  static NAN_MODULE_INIT(Init) {
+  static NAN_MODULE_INIT(Init);
+  static NAN_METHOD(NewInstance);
+
+ private:
+  explicit DTraceConsumer(double);
+  ~DTraceConsumer() {}
+
+  static NAN_METHOD(New);
+  static NAN_METHOD(GetValue);
+
+  static Nan::Persistent<v8::Function> constructor;
+
+  double value_;
+};
+
+
+Nan::Persistent<v8::Function> DTraceConsumer::constructor;
+
+DTraceConsumer::DTraceConsumer(double value = 0) {
+	this->value_ = value;
+}
+
+
+NAN_MODULE_INIT(DTraceConsumer::Init) {
     v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
     tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
     Nan::SetPrototypeMethod(tpl, "getValue", GetValue);
 
-    constructor().Reset(Nan::GetFunction(tpl).ToLocalChecked());
-  }
+    constructor.Reset(Nan::GetFunction(tpl).ToLocalChecked());
+}
 
-  static NAN_METHOD(NewInstance) {
-    v8::Local<v8::Function> cons = Nan::New(constructor());
+
+NAN_METHOD(DTraceConsumer::NewInstance) {
+    v8::Local<v8::Function> cons = Nan::New(constructor);
     double value = info[0]->IsNumber() ? Nan::To<double>(info[0]).FromJust() : 0;
     const int argc = 1;
     v8::Local<v8::Value> argv[1] = {Nan::New(value)};
     info.GetReturnValue().Set(Nan::NewInstance(cons, argc, argv).ToLocalChecked());
-  }
+}
 
-  // Needed for the next example:
-  inline double value() const {
-    return value_;
-  }
+NAN_METHOD(DTraceConsumer::GetValue) {
+    DTraceConsumer* obj = ObjectWrap::Unwrap<DTraceConsumer>(info.Holder());
+    info.GetReturnValue().Set(obj->value_);
+}
 
- private:
-  explicit DTraceConsumer(double value = 0) : value_(value) {}
-  ~DTraceConsumer() {}
-
-  static NAN_METHOD(New) {
-    if (info.IsConstructCall()) {
-      double value = info[0]->IsNumber() ? Nan::To<double>(info[0]).FromJust() : 0;
+NAN_METHOD(DTraceConsumer::New) {
+	if (info.IsConstructCall()) {
+		double value = info[0]->IsNumber() ? Nan::To<double>(info[0]).FromJust() : 0;
       DTraceConsumer * obj = new DTraceConsumer(value);
       obj->Wrap(info.This());
       info.GetReturnValue().Set(info.This());
     } else {
       const int argc = 1;
       v8::Local<v8::Value> argv[argc] = {info[0]};
-      v8::Local<v8::Function> cons = Nan::New(constructor());
+      v8::Local<v8::Function> cons = Nan::New(constructor);
       info.GetReturnValue().Set(Nan::NewInstance(cons, argc, argv).ToLocalChecked());
     }
-  }
-
-  static NAN_METHOD(GetValue) {
-    DTraceConsumer* obj = ObjectWrap::Unwrap<DTraceConsumer>(info.Holder());
-    info.GetReturnValue().Set(obj->value_);
-  }
-
-  static inline Nan::Persistent<v8::Function> & constructor() {
-    static Nan::Persistent<v8::Function> my_constructor;
-    return my_constructor;
-  }
-
-  double value_;
-};
+}
 
 NAN_MODULE_INIT(Init) {
   DTraceConsumer::Init(target);
